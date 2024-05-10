589e4b185afc8a4a534dfa03eb23109d66cdb7a2
Kevin Galligan <kgalligan@gmail.com>
2024-05-10 13:51:37 -0400

    Import

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("multiplatform") version libs.versions.kotlin.get() apply false
+    kotlin("plugin.serialization") version libs.versions.kotlin.get() apply false
+    id("com.android.library") version libs.versions.android.gradle.plugin.get() apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+subprojects {
+    val GROUP: String by project
+    val VERSION_NAME: String by project
+    group = GROUP
+    version = VERSION_NAME
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+include(":app")
+
+/*
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("kmpLibs") {
+            from(files("library/gradle/libs.versions.toml"))
+        }
+    }
+}
+
+include(":app", ":breeds", ":analytics")
+project(":breeds").projectDir = File("library/breeds")
+project(":analytics").projectDir = File("library/analytics")
+*/
+
